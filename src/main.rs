b819f4c5817ea31b3ec4b2c2181e0fe92d3122ae
Christian Blume <chr.blume@gmail.com>
2019-10-06 17:52:11 +1300

    Enable recursive search

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
 extern crate clap;
 use clap::{App, Arg};
-use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::path::Path;
+
+struct Config {
+    pub recursive: bool,
+    pub location: bool,
+}
+
+fn search_file(config: &Config, string: &str, path: &Path) {
+    let filename = path.to_str().unwrap();
+    let file = fs::File::open(filename).expect("Could not open file for reading");
+    let reader = BufReader::new(file);
+    for (i, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        if line.contains(string) {
+            if config.location {
+                println!("[{}:{}]{}", filename, i + 1, line);
+            } else {
+                println!("{}", line);
+            }
+        }
+    }
+}
+
+fn search_dir(config: &Config, string: &str, dir: &Path) {
+    for entry in fs::read_dir(dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            search_file(&config, string, &path);
+        } else if config.recursive && path.is_dir() {
+            search_dir(&config, string, &path);
+        }
+    }
+}
 
 fn main() {
     let matches = App::new("rsgrep")
@@ -15,22 +49,39 @@ fn main() {
                 .required(true),
         )
         .arg(
-            Arg::with_name("filename")
-                .help("The file to search in")
+            Arg::with_name("path")
+                .help("The path to search in (file or directory)")
                 .required(true),
+        )
+        .arg(
+            Arg::with_name("recursive")
+                .short("r")
+                .long("recursive")
+                .help("Whether to search directories recursively")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("location")
+                .short("l")
+                .long("location")
+                .help("Whether to print filename and linenumber")
+                .takes_value(false),
         )
         .get_matches();
 
     let string = matches.value_of("string").unwrap();
-    let filename = matches.value_of("filename").unwrap();
+    let path = matches.value_of("path").unwrap();
+    let recursive = matches.is_present("recursive");
+    let location = matches.is_present("location");
 
-    let file = File::open(filename).expect("Could not open file for reading");
-    let reader = BufReader::new(file);
+    let config = Config{recursive, location};
 
-    for (i, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        if line.contains(string) {
-            println!("{}:{}", i + 1, line);
-        }
+    let path = Path::new(path);
+    if path.is_file() {
+        search_file(&config, string, &path);
+    } else if path.is_dir() {
+        search_dir(&config, string, &path);
+    } else {
+        panic!("Could not open path");
     }
 }
