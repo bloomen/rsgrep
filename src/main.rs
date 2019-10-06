98a57f8fc88ac32e5fc511bc63372e5a6aceea78
Christian Blume <chr.blume@gmail.com>
2019-10-06 19:40:17 +1300

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate clap;
 use clap::{App, Arg};
 use std::fs;
 use std::io::prelude::*;
-use std::io::BufReader;
+use std::io;
 use std::path::Path;
 
 struct Config {
@@ -10,30 +10,67 @@ struct Config {
     pub location: bool,
 }
 
+fn path_to_string(path: &Path) -> String {
+    match path.to_str() {
+        Some(name) => {
+            String::from(name)
+        }
+        None => {
+            String::from("<None>")
+        }
+    }
+}
+
 fn search_file(config: &Config, string: &str, path: &Path) {
-    let filename = path.to_str().unwrap();
-    let file = fs::File::open(filename).expect("Could not open file for reading");
-    let reader = BufReader::new(file);
-    for (i, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        if line.contains(string) {
-            if config.location {
-                println!("[{}:{}]{}", filename, i + 1, line);
-            } else {
-                println!("{}", line);
+    let filename = path_to_string(path);
+    match fs::File::open(path) {
+        Ok(file) => {
+            let reader = io::BufReader::new(file);
+            for (i, line) in reader.lines().enumerate() {
+                match line {
+                    Ok(line) => {
+                        if line.contains(string) {
+                            if config.location {
+                                println!("[{}:{}]{}", filename, i + 1, line);
+                            } else {
+                                println!("{}", line);
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        println!("Error: Reading from file ({}): {}", err, filename);
+                        break;
+                    }
+                }
             }
+        }
+        Err(err) => {
+            println!("Error: Cannot open file ({}): {}", err, filename);
         }
     }
 }
 
 fn search_dir(config: &Config, string: &str, dir: &Path) {
-    for entry in fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_file() {
-            search_file(&config, string, &path);
-        } else if config.recursive && path.is_dir() {
-            search_dir(&config, string, &path);
+    match fs::read_dir(dir) {
+        Ok(mut entries) => {
+            while let Some(entry) = entries.next() {
+                match entry {
+                    Ok(entry) => {
+                        let path = entry.path();
+                        if path.is_file() {
+                            search_file(&config, string, &path);
+                        } else if config.recursive && path.is_dir() {
+                            search_dir(&config, string, &path);
+                        }
+                    }
+                    Err(err) => {
+                        println!("Error: Iterating directory ({}): {}", err, path_to_string(dir));
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            println!("Error: Cannot iterate directory ({}): {}", err, path_to_string(dir));
         }
     }
 }
@@ -70,18 +107,18 @@ fn main() {
         .get_matches();
 
     let string = matches.value_of("string").unwrap();
-    let path = matches.value_of("path").unwrap();
+    let inpath = matches.value_of("path").unwrap();
     let recursive = matches.is_present("recursive");
     let location = matches.is_present("location");
 
     let config = Config{recursive, location};
 
-    let path = Path::new(path);
+    let path = Path::new(inpath);
     if path.is_file() {
         search_file(&config, string, &path);
     } else if path.is_dir() {
         search_dir(&config, string, &path);
     } else {
-        panic!("Could not open path");
+        println!("Error: Cannot open path: {}", inpath);
     }
 }
